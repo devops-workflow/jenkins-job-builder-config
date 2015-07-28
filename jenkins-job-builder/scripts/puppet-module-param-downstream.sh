f39f71ff7a8712255324ccfbe12131564a375efd
Steven Nemetz <steven.nemetz@fireeye.com>
2015-07-28 13:57:24 -0700

    Debugging

--- jenkins-job-builder/scripts/puppet-module-param-downstream.sh
+++ jenkins-job-builder/scripts/puppet-module-param-downstream.sh
@@ -1,20 +1,21 @@
-set -x
+#set -x
 ###
-### TODO: Need t be rewritten
+### TODO: Need to be rewritten
 ###
 # Setup Parameters for next job
 #grab the module URL needed for updating the super-repo:
 echo 'Saving parameters for new job'
 cd $WORKSPACE
-moduleURL=`cat .git/config |grep url`
-moduleURL=`echo $moduleURL |sed 's/url = //g'`
+moduleURL=$(cat .git/config |grep url)
+moduleURL=$(echo $moduleURL |sed 's/url = //g')
 echo "moduleURL=$moduleURL" > parameters.txt
-#branchName=`git branch |grep \* |sed 's/\* //'`
+#branchName=$(git branch |grep \* |sed 's/\* //')
 branchName='master'
 if [[ "${JOB_NAME}" == PuppetDev* ]]; then
-  branchName=`echo ${JOB_NAME} |sed 's/.*-//'`
+  branchName=$(echo ${JOB_NAME} |sed 's/.*-//')
 fi
 
 echo "branchName=$branchName" >> parameters.txt
+echo "DEBUG: done building downstream parameters"
 set +x
 
