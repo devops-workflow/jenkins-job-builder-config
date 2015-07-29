249396795daa8cb97ec673f92c864be554ee24d8
Steven Nemetz <steven.nemetz@fireeye.com>
2015-07-29 13:31:16 -0700

    Start setting up anchorchain file task

--- jenkins-job-builder/scripts/anchor-chain.sh
+++ jenkins-job-builder/scripts/anchor-chain.sh
@@ -1,22 +1,33 @@
-
+set +x
+echo 'Creating/Updating AnchorChain file'
 # Create or modify anchor chain file to create links for a job to its related resources
-anchorfile=anchor
+file_anchorchain=anchor
+dir_icons=userContent/customIcon
 
 # Anchor Chain file format:
 # name, url, icon
 # Site wide icons will be in userContent/customIcon
 
+#if [ ! -f $file_anchorchain ]; then
+  # Create new file
+  # TODO: change to HERE doc
+  #cat <<ANCHOR > $file_anchorchain
 # Ticketing Project
 # Jira
-
+echo "Project Jira,http://url,${dir_icons}/jira-logo.png" >> $file_anchorchain
 # Documentation
 # Confluence
+echo "Project Confluence,http://url,${dir_icons}/confluence-logo.png" >> $file_anchorchain
 
 # Notification channel / chat
 # Hipchat
-<Project Name> Hipchat,http://URL,userContent/customIcon/hipchat-icon.png
+echo "Project Hipchat,http://url,${dir_icons}/hipchat-logo.png" >> $file_anchorchain
 
 # Monitoring
 
 # Metrics
-
+echo "Project Grafana,http://url,${dir_icons}/grafana-logo.png" >> $file_anchorchain
+#else
+  # Update existing file
+  
+# Get Icons: graphite, nagios, elastic search/kibana, 
