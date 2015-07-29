e6555827d1dd374aae8d11748f7d1c554e7d8805
Steven Nemetz <steven.nemetz@fireeye.com>
2015-07-29 16:41:52 -0700

    Cleanup and setup heredoc

--- jenkins-job-builder/scripts/anchor-chain.sh
+++ jenkins-job-builder/scripts/anchor-chain.sh
@@ -1,36 +1,36 @@
 set +x
 echo 'Creating/Updating AnchorChain file'
 # Create or modify anchor chain file to create links for a job to its related resources
+#
+# Ideas of things to link:
+#-----------------
+# Ticketing Project: Jira
+# Documentation: Confluence
+# Notification channel / chat: Hipchat
+# Monitoring: 
+# Metrics: grafana
+
 file_anchorchain=anchor
-#dir_icons=userContent/customIcon
-#dir_icons=customIcon
 dir_icons=/userContent/customIcon
 
 # Anchor Chain file format:
 # name, url, icon
 # Site wide icons will be in userContent/customIcon
-#echo -e &quot;Wiki-SysAdmin\thttps://wiki.com/display/SystemAdmin/Home\thttps://wiki.com/s/1725/1/_/images/logo/confluence_16.png&quot; > links
 
 #if [ ! -f $file_anchorchain ]; then
   # Create new file
   # TODO: change to HERE doc
-  #cat <<ANCHOR > $file_anchorchain
-# Ticketing Project
-# Jira
-echo -e "Project-Jira\thttp://url.com\t${dir_icons}/jira.png" > $file_anchorchain
-# Documentation
-# Confluence
-echo -e "Project-Confluence\thttp://url.com\t${dir_icons}/confluence.png" >> $file_anchorchain
+#  cat <<ANCHOR > $file_anchorchain
+#Project-Jira	http://url.com	${dir_icons}/jira.png
+#Project-Confluence	http://url.com	${dir_icons}/confluence.png
+#Project-Hipchat	http://url.com	${dir_icons}/hipchat.png
+#Project-Grafana	http://url.com	${dir_icons}/grafana.png
+#ANCHOR
 
-# Notification channel / chat
-# Hipchat
-echo -e "Project-Hipchat\thttp://url.com\t${dir_icons}/hipchat.png" >> $file_anchorchain
-
-# Monitoring
-
-# Metrics
-echo -e "Project-Grafana\thttp://url.com\t${dir_icons}/grafana.png" >> $file_anchorchain
+  echo -e "Project-Jira\thttp://url.com\t${dir_icons}/jira.png" > $file_anchorchain
+  echo -e "Project-Confluence\thttp://url.com\t${dir_icons}/confluence.png" >> $file_anchorchain
+  echo -e "Project-Hipchat\thttp://url.com\t${dir_icons}/hipchat.png" >> $file_anchorchain
+  echo -e "Project-Grafana\thttp://url.com\t${dir_icons}/grafana.png" >> $file_anchorchain
 #else
   # Update existing file
-  
-# Get Icons: graphite, nagios, elastic search/kibana, 
+
