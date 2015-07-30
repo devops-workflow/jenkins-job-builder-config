1e4dc4802bcc891ea6bb894c8d2e63a2dbbfcf14
Steven Nemetz <steven.nemetz@fireeye.com>
2015-07-30 11:43:31 -0700

    Working on handling macro objects

--- jenkins-job-builder/scripts/anchor-chain.sh
+++ jenkins-job-builder/scripts/anchor-chain.sh
@@ -37,8 +37,9 @@ dir_icons=/userContent/customIcon
 echo "DEBUG: custom-val={custom-val}"
 echo "DEBUG: custom-obj={custom-obj}"
 # Object is json array
-frag1="'links'"
-frag="{$frag1" # : 
+frag1='{'
+frag2="'links'"
+frag="$frag1$frag2" # : 
 
 json=$(echo "$frag : {custom-obj}}" | sed s/\'/\"/g)
 echo "DEBUG: json=$json"
