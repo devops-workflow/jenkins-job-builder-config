82e1ca396f8a03476d53cc7afe17d23b7e5ca689
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-04 16:05:31 -0700

    Remove old syntax check

--- jenkins-job-builder/scripts/syntax-bash.sh
+++ jenkins-job-builder/scripts/syntax-bash.sh
@@ -7,7 +7,6 @@ set +x
 echo "Syntax check: Bash"
 
 echo 'xxxxxxxxxxxxxxxxx';echo 'Executing the Bash Syntax check:'
-find . -name '*.sh' -type f | xargs -r -n 1 bash -n
 for file in $(find . -name '*.sh' -type f); do
   bash -n $file 2>&1 | while read line; do
     echo "BASH_SYNTAX:$line";
