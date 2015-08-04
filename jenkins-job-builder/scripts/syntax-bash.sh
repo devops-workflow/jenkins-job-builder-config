f04b229e6373454349157833774b896d62892504
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-04 15:56:29 -0700

    Setup for Jenkins warnign parsing of Bash syntax warnings

--- jenkins-job-builder/scripts/syntax-bash.sh
+++ jenkins-job-builder/scripts/syntax-bash.sh
@@ -1,10 +1,16 @@
 set +x
 # Syntax check - Bash
 # bash -n <file.rb>
-# TODO:
-# - create output that can be parsed by Jenkins warnings plugin
+#
+# Output is setup to be parsed by Jenkins warnings plugin
 
 echo "Syntax check: Bash"
 
 echo 'xxxxxxxxxxxxxxxxx';echo 'Executing the Bash Syntax check:'
 find . -name '*.sh' -type f | xargs -r -n 1 bash -n
+for file in $(find . -name '*.sh' -type f); do
+  bash -n $file 2>&1 | while read line; do
+    echo "BASH_SYNTAX:$line";
+  done
+done
+
