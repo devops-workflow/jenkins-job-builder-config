15e5d3cad788938d5bf634579bc158ad33e98a39
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-17 09:42:21 -0700

    Fix for Puppet Enterprise

--- jenkins-job-builder/scripts/syntax-erb.sh
+++ jenkins-job-builder/scripts/syntax-erb.sh
@@ -7,11 +7,13 @@ set +x
 echo 'xxxxxxxxxxxxxxxxx';echo 'Executing the ERB Syntax check:'
 if [ -f /opt/puppet/bin/ruby ]; then
   # Puppet Enterprise
+  ERB='/opt/puppet/bin/erb'
   RUBY='/opt/puppet/bin/ruby'
 else
+  ERB='erb'
   RUBY='ruby'
 fi
-find . -name '*.erb' -type f | xargs -r -n 1 -t erb -P -x -T - | $RUBY -c
+find . -name '*.erb' -type f | xargs -r -n 1 -t $ERB -P -x -T - | $RUBY -c
 
 # Check OpenStack uses:
 # for f in `find . -iname *.erb` ; do
