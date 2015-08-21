701657c579092d1d2d1b1b8994a89716b3dc751d
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-21 13:00:38 -0700

    More comments

--- jenkins-job-builder/scripts/test-rat.pp
+++ jenkins-job-builder/scripts/test-rat.pp
@@ -1,6 +1,11 @@
 set +x
 # Documentation Check - Apache RAT (Release Audit Tool)
-# Check licences
+# Audit Licence Headers
+# https://creadur.apache.org/rat/
+#
+# Can also look at other tools in Creadur suite
+# Apache Tentacles helps to audit in bulk components uploaded to a staging repository.
+# Apache Whisker assists assembled applications maintain correct legal documentation.
 #
 # TODO:
 # - create output that can be parsed by Jenkins warnings plugin
