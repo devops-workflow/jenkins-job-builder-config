8c1291d574953b07fe7957f33b787ca40898b472
Steven Nemetz <steven.nemetz@fireeye.com>
2015-11-04 10:40:56 -0800

    Fix path for puppet enterprise

--- jenkins-job-builder/global/scripts/syntax-puppetfile.sh
+++ jenkins-job-builder/global/scripts/syntax-puppetfile.sh
@@ -2,6 +2,12 @@ set +x
 # Syntax check - Puppetfile
 # r10k puppetfile check <file.pp>
 echo 'xxxxxxxxxxxxxxxxx';echo 'Executing the Puppetfile Syntax check:'
-# /usr/local/bin/r10k
+if [ -f /opt/puppet/bin/r10k ]; then
+  # Puppet Enterprise
+  R10K_BIN='/opt/puppet/bin/r10k'
+else
+  R10K_BIN='r10k'
+  # /usr/local/bin/r10k
+fi
 
-find . -name Puppetfile -type f | xargs -r -n 1 -t r10k puppetfile check
+find . -name Puppetfile -type f | xargs -r -n 1 -t $R10K_BIN puppetfile check
