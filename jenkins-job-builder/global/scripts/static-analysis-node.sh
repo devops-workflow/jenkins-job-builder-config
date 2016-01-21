bea4d8e643b448898a44059a8303950722b20577
Steven Nemetz <steven.nemetz@fireeye.com>
2016-01-21 09:53:28 -0800

    Initial stub for Node.js

---
+++ jenkins-job-builder/global/scripts/static-analysis-node.sh
@@ -0,0 +1,12 @@
+set +x
+# Static Analysis Checks - Node.js
+# Tools - Coverage
+# - coverage
+# Tools - Security:
+# - Node Security Project (nsp)
+# Tools - Misc
+# - jscs, xo, 
+
+echo 'xxxxxxxxxxxxxxxxx';echo 'Executing Node.js Static Analysis testing:'
+echo 'xxxxxxxxxxxxxxxxx';echo 'Executing Node.js Security testing:'
+
